ded1b1eb693eb135e82fbd3973b0184d8657f31a
pawel-stolka <pawel.stolka@gmail.com>
2018-02-13 12:04:28 +0100

    testing d3Service - working!

---
+++ src/app/testd3/testd3.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit, ElementRef } from '@angular/core';
+import { D3Service, D3, Selection } from 'd3-ng2-service';
+
+@Component({
+  selector: 'test-d3',
+  templateUrl: './testd3.component.html',
+  styleUrls: ['./testd3.component.css']
+})
+export class Testd3Component implements OnInit {
+
+  private d3: D3;
+  private parentNativeElement: any;
+  private data: Data[];
+
+  constructor(element: ElementRef, d3Service: D3Service) { 
+    this.d3 = d3Service.getD3();
+    this.parentNativeElement = element.nativeElement;
+  }
+
+  ngOnInit() {
+    let d3 = this.d3;
+    let d3ParentElement: Selection<any, any, any, any>;
+
+    this.data = [
+      { 
+        name: 'paweł',
+        status: 'ok'
+      },
+      { 
+        name: 'gaweł',
+        status: 'nu nu'
+      }
+    ]
+    
+
+    if(this.parentNativeElement !== null)
+    {
+      d3ParentElement = d3.select(this.parentNativeElement);
+
+      d3ParentElement
+        .append("ul")
+        .selectAll('li')
+        .data(this.data)
+        .enter()
+        .append('li')
+        .text(function (d) {
+          return d.name + ': ' + d.status;
+        })
+    }
+  }
+
+}
+
+export class Data {
+  name: string;
+  status: string;
+}
